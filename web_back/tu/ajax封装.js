1b8357e71cbd23d94e731656d11da1b38a1071ed
zhangerxiu <1714161982@qq.com>
2019-12-16 23:16:52 +0800

    大事件第一次上传

---
+++ web_back/tu/ajax封装.js
@@ -0,0 +1,35 @@
+var user = {
+    login: function (options) {
+        $.ajax({
+            type: 'post',
+            url: LOGIN,
+            data: {
+                user_name: options.data.user_name,
+                password: options.data.password
+            },
+            success: function (res) {
+                if (res.code == 200) {
+                    options.success();
+                } else {
+                    options.fail();
+                }
+            }
+        });
+
+    },
+    logout: function (options) {
+        $.ajax({
+            type: 'post',
+            url: LOGOUT,
+            success: function (res) {
+                if (res.code == 200) {
+                    options.success();
+                } else {
+                    options.fail();
+                }
+            }
+        })
+    }
+
+
+}
---
+++ web_back/tu/地址.js
@@ -0,0 +1,3 @@
+var dabaojian = 'http://localhost:8000/'
+var LOGIN = dabaojian + 'admin/login';
+var LOGOUT = dabaojian + 'admin/logout';
